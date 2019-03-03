cd8a6a3d3807d8a6c04be20ab92f1abccb44a186
melbarch <elbarchany@gmail.com>
2019-03-03 22:00:30 +0100

    Add Jumping between questions logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,14 @@ class Quiz extends Component {
     };
   }
 
+  showNextQuestion() {
+    this.setState(state => {
+      return {
+        quiz_position: state.quiz_position + 1
+      };
+    });
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -21,7 +29,10 @@ class Quiz extends Component {
           <QuizEnd />
         ) : (
           <QuizQuestion
-            quiz_question={quizData.quiz_questions[this.state.quiz_position]}
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
           />
         )}
       </div>
