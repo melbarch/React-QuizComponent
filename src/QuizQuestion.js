2d667337040dda4dcd24342f9564ce192cf55e51
melbarch <elbarchany@gmail.com>
2019-03-03 15:10:03 +0100

    Add QuizEnd component

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,7 +6,9 @@ class QuizQuestion extends Component {
     return (
       <main>
         <section>
-          <p>{this.props.quiz_question.instruction_text}</p>
+          <p className="QuizQuestion">
+            {this.props.quiz_question.instruction_text}
+          </p>
         </section>
         <section className="buttons">
           <ul>
