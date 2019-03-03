dc53e31a2d149558e7a8b1660ae5a61481880b6a
melbarch <elbarchany@gmail.com>
2019-03-03 15:14:27 +0100

    fix quizEnd

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,10 +17,13 @@ class Quiz extends Component {
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
-        {isQuizEnd === true && <QuizEnd />}
-        <QuizQuestion
-          quiz_question={quizData.quiz_questions[this.state.quiz_position]}
-        />
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={quizData.quiz_questions[this.state.quiz_position]}
+          />
+        )}
       </div>
     );
   }
